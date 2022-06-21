3561f734538eaa7cc1b0c0c732c7aeadba31fa60
Hassan Fathi Sholqamy <devhassanfathi@gmail.com>
2022-06-22 01:50:07 +0200

    Added: GUI in view

--- src/View/GUI.java
+++ src/View/GUI.java
@@ -1,4 +1,190 @@
 package View;
 
-public class GUI {
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.*;
+
+public class GUI extends JFrame implements ActionListener, MouseListener {
+
+    // Menu Bar
+    private JMenuBar menuBar;
+    private JMenu fileMenu;
+    private JMenuItem loadFileItem;
+    private JMenuItem saveFileItem;
+
+
+    private JSplitPane splitPane;
+    // Left Panel
+    private JPanel leftPanel;
+
+    private DefaultTableModel invoicesTableModel;
+    private JTable invoicesTable;
+    private String[] invoicesTableColumns = {"No.", "Date", "Customer", "Total"};
+//    private String[][] invoicesTableData = csvToArray("Sales-Invoice-Generator/src/InvoiceHeader.csv", false);
+    private String [][] invoicesTableData = {{"1", "2", "3", "4"}};
+    String[][] tempData2 = new String[5][100];
+
+    private JButton newInvoiceButton;
+    private JButton deleteInvoiceButton;
+
+    // Right Panel
+    private JPanel rightPanel;
+    private String invoiceNumber;
+
+    private JTextField invoiceDateTF;
+    private JTextField customerNameTF;
+
+    private int invoiceTotal;
+
+    DefaultTableModel invoiceDetailsTableModel;
+    private JTable invoiceDetailsTable;
+    private String[] invoiceDetailsTableColumns = {"No.", "Item Name", "Item Price", "Count", "Item Total"};
+//    private String[][] invoiceDetailsData = csvToArray("Sales-Invoice-Generator/src/InvoiceLine.csv", true);
+    private String [][] invoiceDetailsData = {{"1", "2", "3", "4", "5"}};
+    private JButton saveButton;
+    private JButton cancelButton;
+
+
+    public GUI(){
+        super("Sales Invoice Generator");
+        setLayout(null);
+        setSize(870,700);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // Menu Bar
+        menuBar = new JMenuBar();
+
+        fileMenu = new JMenu("File");
+
+        loadFileItem = new JMenuItem("Load File", 'L');
+        loadFileItem.setAccelerator(KeyStroke.getKeyStroke('O', KeyEvent.CTRL_DOWN_MASK));
+        loadFileItem.addActionListener(this);
+        loadFileItem.setActionCommand("loadFile");
+        fileMenu.add(loadFileItem);
+
+        saveFileItem = new JMenuItem("Save File", 'S');
+        saveFileItem.setAccelerator(KeyStroke.getKeyStroke('S',KeyEvent.CTRL_DOWN_MASK));
+        saveFileItem.addActionListener(this);
+        saveFileItem.setActionCommand("saveFile");
+        fileMenu.add(saveFileItem);
+
+        setJMenuBar(menuBar);
+        menuBar.add(fileMenu);
+        //++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        //Left Panel
+        invoicesTableModel = new DefaultTableModel(invoicesTableData, invoicesTableColumns);
+        invoicesTable = new JTable(invoicesTableModel);
+        invoicesTable.addMouseListener(this);
+        JScrollPane invoicesTableSP = new JScrollPane(invoicesTable);
+        invoicesTableSP.setBounds(20,30,400,540);
+        add(invoicesTableSP);
+
+        newInvoiceButton = new JButton("Create New Invoice");
+        newInvoiceButton.setBounds(70, 590, 150,20);
+        newInvoiceButton.addActionListener(this);
+        newInvoiceButton.setActionCommand("newInvoice");
+        add(newInvoiceButton);
+
+        deleteInvoiceButton = new JButton("Delete Invoice");
+        deleteInvoiceButton.setBounds(230, 590, 150,20);
+        deleteInvoiceButton.addActionListener(this);
+        deleteInvoiceButton.setActionCommand("deleteInvoice");
+        add(deleteInvoiceButton);
+
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        // Right Panel
+        JLabel invoiceNumberLabel = new JLabel("Invoice Number: "+ invoiceNumber);
+        invoiceNumberLabel.setBounds(440,20, 400, 20);
+        add(invoiceNumberLabel);
+
+        JLabel invoiceDateLabel = new JLabel("Invoice Date");
+        invoiceDateLabel.setBounds(440,60, 100, 20);
+        add(invoiceDateLabel);
+
+        invoiceDateTF = new JTextField(20);
+        invoiceDateTF.setBounds(540,60, 300, 20);
+        add(invoiceDateTF);
+
+        JLabel customerNameLabel = new JLabel("Customer Name");
+        customerNameLabel.setBounds(440,100, 100, 20);
+        add(customerNameLabel);
+
+        customerNameTF = new JTextField(20);
+        customerNameTF.setBounds(540,100, 300, 20);
+        add(customerNameTF);
+
+        JLabel invoiceTotalLabel = new JLabel("Invoice Total: " + invoiceTotal);
+        invoiceTotalLabel.setBounds(440,140, 400, 20);
+        add(invoiceTotalLabel);
+
+        rightPanel = new JPanel(null);
+        rightPanel.setBounds(440,180,400,400);
+
+        Border blacklineRight = BorderFactory.createTitledBorder("Invoice Items"); // Create border for the panel with Title
+        rightPanel.setBorder(blacklineRight);
+        getContentPane().add(rightPanel, BorderLayout.CENTER);
+        add(rightPanel);
+
+        invoiceDetailsTableModel = new DefaultTableModel(invoiceDetailsData, invoiceDetailsTableColumns);
+        invoiceDetailsTable = new JTable(invoiceDetailsTableModel);
+        JScrollPane invoiceDetailsTableSP = new JScrollPane(invoiceDetailsTable);
+        invoiceDetailsTableSP.setBounds(10,20,380,370);
+        rightPanel.add(invoiceDetailsTableSP, BorderLayout.CENTER);
+
+        saveButton = new JButton("Save");
+        saveButton.setBounds(500,590,100,20);
+        saveButton.addActionListener(this);
+        saveButton.setActionCommand("saveNewInvoice");
+        add(saveButton);
+
+        cancelButton = new JButton("Cancel");
+        cancelButton.setBounds(670,590,100,20);
+        add(cancelButton);
+    }
+
+
+
+
+
+
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
+
+
